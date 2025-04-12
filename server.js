41fadc95031b0f996b71b0ce0d5e07d164058cf7
desivar <jillian_desire@yahoo.es>
2025-04-11 22:18:17 -0600

    server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const connectDB = require('./db');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+connectDB();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('OnlineSystem API is running!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
