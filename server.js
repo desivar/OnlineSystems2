8fa732b2a1e60019677e8dce4c7dce5a03becec8
desivar <jillian_desire@yahoo.es>
2025-04-11 22:22:37 -0600

    update server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const connectDB = require('./db');
 const cors = require('cors');
+const usersRoute = require('./routes/users');
 require('dotenv').config();
 
 const app = express();
@@ -11,6 +12,8 @@ connectDB();
 
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use('/users', usersRoute);
 
 app.get('/', (req, res) => {
   res.send('OnlineSystem API is running!');
