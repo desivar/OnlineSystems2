d59e03426e11e7b94418b4b75fb4ee6af226c80f
desivar <jillian_desire@yahoo.es>
2025-04-12 02:09:14 -0600

    fix deprecade warning

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const connectDB = async () => {
   try {
     console.log('MONGO_URI:', process.env.MONGO_URI);
-    await mongoose.connect(process.env.MONGO_URI); // Remove the options
+    await mongoose.connect(process.env.MONGO_URI); // Remove both options
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
