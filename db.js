449d0063d2235bf2f935bf2a0a35ddfe75093887
desivar <jillian_desire@yahoo.es>
2025-04-12 02:06:26 -0600

    db fix error

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    console.log('MONGO_URI:', process.env.MONGO_URI); // Add this line for debugging.
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
