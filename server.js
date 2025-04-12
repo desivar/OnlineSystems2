1d5ab4c43e55951ea4ab79a152d398bf43532325
desivar <jillian_desire@yahoo.es>
2025-04-11 22:26:59 -0600

    SERVER.JS UPADTE

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const usersRoute = require('./routes/users');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5500;
 
 connectDB();
 
